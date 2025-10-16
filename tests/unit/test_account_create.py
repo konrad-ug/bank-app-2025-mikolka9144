644cfaab6069a365ef4e99c08f64b280572e5fb1
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-10-16 15:07:51 +0200

    du a feature 1

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -6,3 +6,9 @@ class TestAccount:
         account = Account("John", "Doe")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0
+    def test_account_creation_with_balance(self):
+        account = Account("Jane", "Doe", 100)
+        assert account.first_name == "Jane"
+        assert account.last_name == "Doe"
+        assert account.balance == 100

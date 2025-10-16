236fb30a6014a2a306fda90647addfa6eac8095a
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-10-16 15:11:40 +0200

    feature 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -7,8 +7,6 @@ class TestAccount:
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
-    def test_account_creation_with_balance(self):
-        account = Account("Jane", "Doe", 100)
-        assert account.first_name == "Jane"
-        assert account.last_name == "Doe"
-        assert account.balance == 100
+    def test_account_creation_with_pesel(self):
+        account = Account("Jane", "Doe", 100,pesel="12345678901")
+        assert account.pesel == "12345678901"
